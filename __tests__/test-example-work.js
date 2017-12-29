b70b619f6913c8e772ac5b3c7d164daefa5df9f4
Chris Williams <chrilliams@gmail.com>
2017-12-29 16:05:34 +0000

    fix - Show a failed test in codepipeline

--- __tests__/test-example-work.js
+++ __tests__/test-example-work.js
@@ -51,7 +51,6 @@ describe("ExampleWork component", () => {
 
     it("Should have the image src set correctly", () => {
       expect(images.getElement().props.src).toEqual(myWork[1].image.src);
-      expect(5).toEqual(6);
     })
 
 
